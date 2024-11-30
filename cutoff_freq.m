915a6645ed907edd92cce99848613a452b31dd57
BohoWenju <45180508+BohoWenju@users.noreply.github.com>
2024-11-30 17:00:43 +0100

    Updated cutoff_freq.m

--- cutoff_freq.m
+++ cutoff_freq.m
@@ -1,22 +1,19 @@
 %% CutOff Frequency.
 % Both findings follow the same logic.
-% 1) Interpolate both phase and frequency vector.
-% 2) Find where phase is below -180 for the first time.
+% 1) Interpolate both gain and frequency vector.
+% 2) Find where gain is below 1 for the first time.
 % 3) Get the freq at that point.
-function W_cutoff = cutoff_freq(W, Apmn)
+function W_cutoff = cutoff_freq(W, Mpmn)
     W2   = interp(W, 100); 
     Apmn2 = interp(Apmn, 100);
-    below_180_idx = find(Apmn2 <= -180, 1, 'first'); % Obviously the point above
-    % Should be in index below_180_idx - 1.
+    below_zero = find(Apmn2 <= 1, 1, 'first'); 
 
-    % If no phase reaches -180, gain margin is theoretically infinite
-    if isempty(below_180_idx)
+    if isempty(below_zero)
         W_cutoff = Inf;
         return;
     end
 
-   % To calculate the phase margin/gain margin just get the
-   % difference between the corresponding boint and the -180deg/0.
-   
-   W_cutoff = W2(below_180_idx);
+   % To find the cutoff frequency just get the corresponding frequency for
+   % amplitude = 1.
+   W_cutoff = W2(below_zero);
 end
